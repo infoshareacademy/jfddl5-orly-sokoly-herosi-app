704d67fb4b37e5c7d2979d3e7967a0a825478358
Magda Kłopotek <magda.klopotek@gmail.com>
2018-07-24 15:47:00 +0200

    AppBar and Router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom' 
+import AppBar from 'material-ui/AppBar'
 
 class App extends React.Component {
   render() {
     return (
-      <div> Cokolwiek
+      <div>
+        <Router>
+          <AppBar 
+            title={`Survey App`}
+          />
+        </Router>
       </div>
     );
   }
