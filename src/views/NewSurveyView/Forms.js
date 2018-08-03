140f098f50d32badaf6cc3f640774727a1643065
MateuszPeliński <pelinski.m@gmail.com>
2018-08-03 15:31:50 +0200

    added files

---
+++ src/views/NewSurveyView/Forms.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
+import PaperRefined from '../../components/PaperRefined'
+
+
+const Forms = (props) => (
+    <PaperRefined>
+        <TextField
+            type={'text'}
+            value={props.newTaskText}
+            fullWidth={true}
+            onChange={props.onNewTaskTextChanged}
+        />
+        <RaisedButton
+            label={'Dodaj'}
+            primary={true}
+            fullWidth={true}
+            onClick={props.onAddNewTaskClickHandler}
+        />
+    </PaperRefined>
+)
+
+export default Forms
