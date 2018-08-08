2f549a6717449c3ebe35142dedc0aa9a177aba2e
MateuszPeliński <pelinski.m@gmail.com>
2018-08-08 21:42:08 +0200

    added material ui

--- src/views/NewSurveyView/NewSurveyView.js
+++ src/views/NewSurveyView/NewSurveyView.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import TextField from 'material-ui/TextField'
+import OSHPaper from '../../components/OSHPaper'
+import RaisedButton from 'material-ui/RaisedButton'
 
 const myApiUrl = 'https://survey-app-84f53.firebaseio.com/surveys'
 
@@ -10,47 +13,74 @@ class NewSurveyView extends React.Component {
 
     createHandler = () => {
         const request = {
-          method: 'POST',
-          body: JSON.stringify({
-            title: this.state.title,
-            text: this.state.text,
-          })
+            method: 'POST',
+            body: JSON.stringify({
+                title: this.state.title,
+                text: this.state.text,
+            })
         }
         fetch(`${myApiUrl}.json`, request)
-          .then(response => response.json())
-          .then(data => {
-            this.setState({
-              title: '',
-              text: '',              
-            })            
-          })
-      }
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    title: '',
+                    text: '',
+                })
+            })
+    }
 
-      titleChange = (event) => {
+    titleChange = (event) => {
         const fieldTitle = event.target.value
         this.setState({
             title: fieldTitle
         });
-      }
-      textChange = (event) => {
+    }
+    textChange = (event) => {
         const fieldText = event.target.value
         this.setState({
             text: fieldText
         });
-      }
-
-
-    render() {
-        return (
-            <div>
-                Title:<input type="text" name="title" onChange={this.titleChange} value={this.state.title} />
-                Your text:<input type="text" name="text-field" onChange={this.textChange} value={this.state.text} />
-                <button onClick={this.createHandler}>Create</button>
-            </div>
-        )
     }
+    
+    render() {
+        return ( 
+            <div >
+                <OSHPaper >
+                <h1>Create new survey</h1>
+                    < TextField 
+                        hintText = "Enter title"
+                        floatingLabelText = "Survey title"
+                        fullWidth = {true}
+                        type = "text"
+                        name = "title"
+                        onChange = {this.titleChange}
+                        value = {this.state.title}
+                        /><br/>
+                    < TextField 
+                        hintText = "Enter contents survey"
+                        floatingLabelText = "Content"
+                        fullWidth = {true} 
+                        type = "text"
+                        name = "text-field"
+                        onChange = {this.textChange}
+                        value = {this.state.text}
+                    /> 
+                    <RaisedButton 
+                        label="Create Survey" 
+                        fullWidth = {true}
+                        primary={true} 
+                        style={style} 
+                        onClick = {this.createHandler} 
+                    /> 
+            </OSHPaper> 
+        </div>
+    )
+}
 
 
 }
+const style = {
+    margin: 12,
+  };
 
 export default NewSurveyView
