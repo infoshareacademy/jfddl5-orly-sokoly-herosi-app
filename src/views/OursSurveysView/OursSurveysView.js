6755dae4bb22c5558d2a78d6102947d15fff97dd
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-08 16:02:24 +0200

    merge after bug

--- src/views/OursSurveysView/OursSurveysView.js
+++ src/views/OursSurveysView/OursSurveysView.js
@@ -76,34 +76,4 @@ class OursSurveysView extends React.Component {
 }
 
 
-class OursSurveysView extends React.Component {
-    state = {
-        survey: [{
-            title: 'jeb z dzidy',
-            text: 'Kacpeł i puchałke',
-            id: Date.now(),
-            isFavourite: false
-        },
-        {
-            title: 'dupa',
-            text: 'Prosiałke i puchałke',
-            id: Date.now(),
-            isFavourite: false
-        },
-        {
-            title: '',
-            text: '',
-            id: Date.now(),
-            isFavourite: false
-        }]
-    }
-
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
-}
 export default OursSurveysView
