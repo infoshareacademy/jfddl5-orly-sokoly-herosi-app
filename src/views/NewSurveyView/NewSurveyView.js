83d25f4ba501d8da1233ab2472435b12add52b41
Mateusz Peliński <pelinski.m@gmail.com>
2018-08-08 16:35:18 +0200

    made adde new surveyto firebase

--- src/views/NewSurveyView/NewSurveyView.js
+++ src/views/NewSurveyView/NewSurveyView.js
@@ -1,7 +1,56 @@
 import React from 'react'
 
-const NewSurveyView = () => (
-    <h1>New Survey View</h1>
-)
+const myApiUrl = 'https://survey-app-84f53.firebaseio.com/surveys'
+
+class NewSurveyView extends React.Component {
+    state = {
+        title: '',
+        text: ''
+    }
+
+    createHandler = () => {
+        const request = {
+          method: 'POST',
+          body: JSON.stringify({
+            title: this.state.title,
+            text: this.state.text,
+          })
+        }
+        fetch(`${myApiUrl}.json`, request)
+          .then(response => response.json())
+          .then(data => {
+            this.setState({
+              title: '',
+              text: '',              
+            })            
+          })
+      }
+
+      titleChange = (event) => {
+        const fieldTitle = event.target.value
+        this.setState({
+            title: fieldTitle
+        });
+      }
+      textChange = (event) => {
+        const fieldText = event.target.value
+        this.setState({
+            text: fieldText
+        });
+      }
+
+
+    render() {
+        return (
+            <div>
+                Title:<input type="text" name="title" onChange={this.titleChange} value={this.state.title} />
+                Your text:<input type="text" name="text-field" onChange={this.textChange} value={this.state.text} />
+                <button onClick={this.createHandler}>Create</button>
+            </div>
+        )
+    }
+
+
+}
 
 export default NewSurveyView
