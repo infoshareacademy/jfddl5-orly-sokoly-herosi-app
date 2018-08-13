6efa3adcd157de5adb7464c86b79294bbbc5638a
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-13 15:32:13 +0200

    searching by category added

---
+++ src/views/OursSurveysView/Category.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+class Category extends Component {
+    render() {
+        return (
+            <div>
+                <SelectField
+                    fullWidth={true}
+                    floatingLabelText="Survey category"
+                    value={this.props.currentCategory}
+                    onChange={this.props.onCategoryChangeHandler}
+                >
+                    <MenuItem value={"All"} primaryText="All" />
+                    <MenuItem value={"People"} primaryText="People" />
+                    <MenuItem value={"Research"} primaryText="Research" />
+                    <MenuItem value={"Alkohols"} primaryText="Alkohols" />
+                    <MenuItem value={"Hobby"} primaryText="Hobby" />
+                    <MenuItem value={"Work"} primaryText="Work" />
+                </SelectField>
+            </div>
+        )
+    }
+}
+export default Category
