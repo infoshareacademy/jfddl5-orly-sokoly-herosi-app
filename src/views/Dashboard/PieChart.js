cdb04cf04043625e843e4b269628f0a0c33d488f
Kacper Swietlaga <kacperres@gmail.com>
2018-08-15 16:51:32 +0200

    added first responsive chart

--- src/views/Dashboard/PieChart.js
+++ src/views/Dashboard/PieChart.js
@@ -3,37 +3,54 @@ import { PieChart as Chart, Pie, Tooltip } from 'recharts'
 
 
 const data = [
-    {
-      value: 30,
-      name: 'nazwa 3'
-    },
-    {
-      value: 40,
-      name: 'nazwa 2'
-    },
-    {
-      value: 27,
-      name: 'nazwa 1'
-    },{
-        value: 27,
-        name: 'nazwa 4'
-      }
-  ]
+  {
+    value: 30,
+    name: 'nazwa 3'
+  },
+  {
+    value: 40,
+    name: 'nazwa 2'
+  },
+  {
+    value: 27,
+    name: 'nazwa 1'
+  }, {
+    value: 27,
+    name: 'nazwa 4'
+  }
+]
 
-const PieChart =() =>(
-    <div>
-    <Chart width={500} height={500}>
-      <Pie 
-        data={data}
-        dataKey="value"
-        nameKey="name"
-        fill="#3366FF" 
-      />
-      <Tooltip />
-    </Chart>
-    </div>
+class PieChart extends React.Component {
+  state = {
+    chartWidth: 500,
+    chartHeight: 500
+  }
+
+  componentDidMount() {
+    this.setState({
+      chartWidth: ( window.innerWidth) / 2,
+      chartHeight:  (window.innerHeight) /2
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Chart width={this.state.chartWidth} height={this.state.chartHeight}>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            fill="#3366FF"
+          />
+          <Tooltip />
+        </Chart>
+      </div>
+
+    )
+  }
 
 
-)
+}
 
 export default PieChart
